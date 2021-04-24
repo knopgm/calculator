0afa0f31614db66d7d80060afb0a028d9c0ab4e4
Gabriela Knop Marques <knop.gm@gmail.com>
2021-04-24 22:36:24 +0200

    Refactor Buttons component to be smaller and to have configurable buttons

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,129 +1,51 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
+import classNames from "classnames";
 
 import "./style.scss";
 
-export function Buttons(props) {
-  const handleReset = () => {
-    props.onButtonClick({ type: "reset" });
-  };
+export function Buttons({ onButtonClick }) {
+  const handleClick = useCallback(
+    (type, value) => onButtonClick({ type, value }),
+    [onButtonClick]
+  );
 
-  const handleOperationDiv = () => {
-    props.onButtonClick({ type: "operation", value: "/" });
-  };
-
-  const handleOperationMul = () => {
-    props.onButtonClick({ type: "operation", value: "*" });
-  };
-
-  const handleNumberSeven = () => {
-    props.onButtonClick({ type: "number", value: 7 });
-  };
-
-  const handleNumberEight = () => {
-    props.onButtonClick({ type: "number", value: 8 });
-  };
-
-  const handleNumberNine = () => {
-    props.onButtonClick({ type: "number", value: 9 });
-  };
-
-  const handleOperationMin = () => {
-    props.onButtonClick({ type: "operation", value: "-" });
-  };
-
-  const handleNumberFour = () => {
-    props.onButtonClick({ type: "number", value: 4 });
-  };
-
-  const handleNumberFive = () => {
-    props.onButtonClick({ type: "number", value: 5 });
-  };
-
-  const handleNumberSix = () => {
-    props.onButtonClick({ type: "number", value: 6 });
-  };
-
-  const handleOperationAdd = () => {
-    props.onButtonClick({ type: "operation", value: "+" });
-  };
-
-  const handleNumberOne = () => {
-    props.onButtonClick({ type: "number", value: 1 });
-  };
-
-  const handleNumberTwo = () => {
-    props.onButtonClick({ type: "number", value: 2 });
-  };
-
-  const handleNumberThree = () => {
-    props.onButtonClick({ type: "number", value: 3 });
-  };
-
-  const handleOperationEqual = () => {
-    props.onButtonClick({ type: "equalOperation", value: "=" });
-  };
-
-  const handleNumberZero = () => {
-    props.onButtonClick({ type: "number", value: 0 });
-  };
-
-  const handleOperationDot = () => {
-    props.onButtonClick({ type: "dot", value: "." });
-  };
+  // prettier-ignore
+  const buttons = useMemo(() => [
+    { label: "AC", type: "reset", handler: handleClick.bind(null, "reset") },
+    { label: "/", type: "operation", handler: handleClick.bind(null, "operation", "/") },
+    { label: "x", type: "operation", handler: handleClick.bind(null, "operation", "*") },
+    { label: "7", type: "number", handler: handleClick.bind(null, "number", 7) },
+    { label: "8", type: "number", handler: handleClick.bind(null, "number", 8) },
+    { label: "9", type: "number", handler: handleClick.bind(null, "number", 9) },
+    { label: "-",  type: "operation", handler: handleClick.bind(null, "operation", "-") },
+    { label: "4", type: "number", handler: handleClick.bind(null, "number", 4) },
+    { label: "5", type: "number", handler: handleClick.bind(null, "number", 5) },
+    { label: "6", type: "number", handler: handleClick.bind(null, "number", 6) },
+    { label: "+", type: "operation", handler: handleClick.bind(null, "operation", "+") },
+    { label: "1", type: "number", handler: handleClick.bind(null, "number", 1) },
+    { label: "2", type: "number", handler: handleClick.bind(null, "number", 2) },
+    { label: "3", type: "number", handler: handleClick.bind(null, "number", 3) },
+    { label: "=", type: "evaluate", handler: handleClick.bind(null, "evaluate") },
+    { label: "0", type: "number", handler: handleClick.bind(null, "number", 0) },
+    { label: ".", type: "dot", handler: handleClick.bind(null, "dot", ".") },
+  ], [handleClick]);
 
   return (
     <div className="buttons-grid">
-      <button className="button button-ac" onClick={handleReset}>
-        AC
-      </button>
-      <button className="button button-op-color" onClick={handleOperationDiv}>
-        /
-      </button>
-      <button className="button button-op-color" onClick={handleOperationMul}>
-        x
-      </button>
-      <button className="button" onClick={handleNumberSeven}>
-        7
-      </button>
-      <button className="button" onClick={handleNumberEight}>
-        8
-      </button>
-      <button className="button" onClick={handleNumberNine}>
-        9
-      </button>
-      <button className="button button-op-color" onClick={handleOperationMin}>
-        -
-      </button>
-      <button className="button" onClick={handleNumberFour}>
-        4
-      </button>
-      <button className="button" onClick={handleNumberFive}>
-        5
-      </button>
-      <button className="button" onClick={handleNumberSix}>
-        6
-      </button>
-      <button className="button button-op-color" onClick={handleOperationAdd}>
-        +
-      </button>
-      <button className="button" onClick={handleNumberOne}>
-        1
-      </button>
-      <button className="button" onClick={handleNumberTwo}>
-        2
-      </button>
-      <button className="button" onClick={handleNumberThree}>
-        3
-      </button>
-      <button className="button button-equal" onClick={handleOperationEqual}>
-        =
-      </button>
-      <button className="button button-zero" onClick={handleNumberZero}>
-        0
-      </button>
-      <button className="button" onClick={handleOperationDot}>
-        .
-      </button>
+      {buttons.map((buttonConfig) => (
+        <button
+          key={buttonConfig.label}
+          className={classNames("button", {
+            "button--operation": buttonConfig.type === "operation",
+            "button--reset": buttonConfig.type === "reset",
+            "button--zero": buttonConfig.label === "0",
+            "button--equal": buttonConfig.type === "evaluate",
+          })}
+          onClick={buttonConfig.handler}
+        >
+          {buttonConfig.label}
+        </button>
+      ))}
     </div>
   );
 }
